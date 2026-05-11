e212e6e78bcfa43c2a62a9a9d90e628dee567ff3
TLEV <timleo.vil@gmail.com>
2026-05-11 09:24:33 +0800

    Initial code for s61-67

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import "./main.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+
+createApp(App).mount("#app");
