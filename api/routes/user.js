c7e5856b7806d3445d1dcc70bdad508eb6203dc2
TLEV <timleo.vil@gmail.com>
2026-06-05 13:51:40 +0800

    Added blank swagger jsdoc comments to user routes

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,14 +1,37 @@
-const express = require('express');
-const userController = require('../controllers/user');
+const express = require("express");
+const userController = require("../controllers/user");
 const { verify } = require("../auth");
 
 const router = express.Router();
 
+/**
+ *  @openapi
+ */
 router.post("/check-email", userController.checkEmailExists);
+
+/**
+ *  @openapi
+ */
 router.post("/register", userController.registerUser);
+
+/**
+ *  @openapi
+ */
 router.post("/login", userController.loginUser);
+
+/**
+ *  @openapi
+ */
 router.get("/details", verify, userController.getProfile);
-router.post('/reset-password', verify, userController.resetPassword);
-router.put('/profile', verify, userController.updateProfile);
+
+/**
+ *  @openapi
+ */
+router.post("/reset-password", verify, userController.resetPassword);
+
+/**
+ *  @openapi
+ */
+router.put("/profile", verify, userController.updateProfile);
 
 module.exports = router;
